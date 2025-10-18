b371f03c9d187e17886d8ea11d44b98ffc3f945a
MarioSg05 <msuyen-2022501@kinal.edu.gt>
2025-10-17 23:23:09 -0600

    Subiendo Enums

---
+++ Enums.java
@@ -0,0 +1,36 @@
+/**
+ * Clase que contiene las enumeraciones
+ */
+public class Enums {
+    
+    /**
+     * Enumeración para el tipo de citas médicas.
+     */
+    public enum TipoCita {
+        CONSULTA_GENERAL,
+        CIRUGIA,
+        TERAPIA,
+        DIAGNOSTICO,
+        ADMINISTRATIVA
+    }
+
+    /**
+     * Enumeración para el estado actual de las citas.
+     */
+    public enum EstadoCita {
+        PROGRAMADA,
+        CONFIRMADA,
+        EN_PROGRESO,
+        COMPLETADA,
+        CANCELADA,
+        REAGENDADA
+    }
+
+    /**
+     * Enumeración para el tipo de turno del personal de enfermería
+     */
+    public enum TipoTurno {
+        DIURNO,
+        NOCTURNO
+    }
+}
